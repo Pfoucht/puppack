ff818c4ba3ba75f46080337f3cac27f28583dc58
Camden Foucht <foucht@gmail.com>
2018-07-07 23:02:49 -0700

    Password encryption

--- server/model/user.js
+++ server/model/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
+const saltRounds = 10;
 
 
 
@@ -17,6 +19,42 @@ const userSchema = new Schema({
     images: [ { type: String } ], // Decide on how to handle images, imageCDN, or store image itself
 
 });
+
+
+// Encryption & Decryption
+
+
+userSchema.pre('save', function(next){
+    var user = this;
+
+    if(!user.isModified('passowrd')) return next();
+
+    bcrypt.hash(user.password, saltRounds, function(err, hash){
+        if(err) return next(err);
+        // Overide text password with hash
+        user.password = hash;
+        next();
+    });
+});
+
+
+
+
+userSchema.methods.comparePassword = function(userPassword, cb){
+    bcrypt.compare(userPassword, this.password, function(err, res){
+        if (err) return cb(err);
+        cb(null, res);
+    });
+};
+
+// Example usage of comparePassword
+// user.comparePassword('password123', function(err, isMatch){
+//     if ( err ) throw err;
+//     console.log(isMatch)  ----> true
+// }
+
+
+
 
 
 module.exports = mongoose.model('User', userSchema);
